c4c9e9c209f9e47f9ba2aafb540ec1642c3a3ba0
WaddySam <wadhahfadel@yahoo.com>
2019-01-29 16:20:32 -0500

    The first commit

---
+++ Ace Club/Ace Club/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Ace Club
+//
+//  Created by Wadker on 1/29/19.
+//  Copyright © 2019 Stairs Academy. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
